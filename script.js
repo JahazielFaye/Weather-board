1890980abf7c56eedc3b157a838846831a0e7881
zxielle <@JahazielFaye>
2022-12-08 16:40:58 -0500

    js updated

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let city = document.querySelector('.city_weather');
+let day = document.querySelector('.weather_day');
+let humidity = document.querySelector('.weather_indicator--humidty.>value');
+let wind = document.querySelector('.weather_indicator--wind');
+let pressure = document.querySelector('.weather_indicator--pressure');
+let image = document.querySelector('.weather_image');
+let temp = document.querySelector('.weather_temperature');
 let weatherAPIKey = '0241c01f3784a713363c767ae4ed4108';
 let weatherBase =''
 
-let getWeatherByCityName = async (city) => {
+let getWeatherByCity = async (city) => {
     let endpoint = weatherBaseEndPoint + '&q=' + city;
     let response = await fetch(endpoint);
     let weather = await response.json();
+
+    console.log(weather);
 }
+
+getWeatherByCity('Florida');
