a93a0d930feeea327c88a5900f18ccd926249b78
zxielle <@JahazielFaye>
2023-02-14 01:33:03 -0500

    Comments & updates

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let search = document.querySelector(".search_weather");
-const city = document.querySelector('.city_weather');
-const day = document.querySelector('.weather_day');
-const humidity = document.querySelector('.weather__indicator--humidity .value');
-const pressure = document.querySelector('.weather__indicator--pressure .value');
-const wind = document.querySelector('.weather__indicator--wind .value');
-const temperature = document.querySelector('.weather_temperature .value');
+let searchba = document.querySelector(".search_weather");
+let city = document.querySelector('.city_weather');
+let day = document.querySelector('.weather_day');
+let humidity = document.querySelector('.weather_indicator--humidity>.value');
+let pressure = document.querySelector('.weather_indicator--pressure>.value');
+let wind = document.querySelector('.weather_indicator--wind>.value');
 let image = document.querySelector(".weather_image");
+let temperature = document.querySelector('.weather_temperature>.value');
 let forecastBlock = document.querySelector(".weather_forecast");
 let suggestions = document.querySelector("#suggestions");
 let weatherAPIKey = "0241c01f3784a713363c767ae4ed4108";
@@ -17,103 +17,45 @@ let forecastBaseEndpoint =
   weatherAPIKey;
 let cityBaseEndpoint = "https://api.teleport.org/api/cities/?search=";
 
-let city_weather = async (city) => {
-    let cityString = city;
-    if (cityString.includes(", ")) {
-      city =
-        cityString.substring(0, cityString.indexOf(", ")) +
-        cityString.substring(cityString.lastIndexOf(", "));
-    } else {
-      city = cityString;
-    }
-  
-    let endpoint = weatherBaseEndpoint + "&q=" + city;
+let getWeatherByCityName = async (city) => {
+    let endpoint = weatherBaseEndpoint + '&q=' + city;
     let response = await fetch(endpoint);
-    if (response.status !== 200) {
-      alert("City not found!");
-      return;
-    }
     let weather = await response.json();
     return weather;
-  };
-  
-  let getForecastByCityID = async (id) => {
-    let endpoint = forecastBaseEndpoint + "&id=" + id;
-    let result = await fetch(endpoint);
-    let forecast = await result.json();
-    let forecastList = forecast.list;
-    let daily = [];
-  
-    forecastList.forEach((day) => {
-        if (typeof day.dt_text === 'string') {
-          let date = new Date(day.dt_text.replace(" ", "T"));
-          let hours = date.getHours();
-          if (hours === 12) {
-            daily.push(day);
-          }
-        }
-      });
-      
-    return daily;
-  };
-  function updateForecast(forecast) {
-    let city_weather = async (city) => {
-        let weather = await getWeatherByCity(city);
-        if (!weather) {
-          return;
-        }
-        let cityID = weather.id;
-        updateCurrentWeather(weather);
-        let forecast = await getForecastByCityID(cityID);
-        updateForecast(forecast);
-      };
   }
-  
-  let init = () => {};
-  
-  search.addEventListener("keydown", async (e) => {
-    if (e.keyCode === 13) {
-      city_weather(search.value);
+
+  searchba.addEventListener('keydown', async (e) => {
+    if(e.keyCode === 13) {
+      let weather = await getWeatherByCityName(searchba.value);
+      updateCurrentWeather(weather);
     }
-  });
-  
-  search.addEventListener("input", async () => {
-    let endpoint = cityBaseEndpoint + search.value;
-    let result = await (await fetch(endpoint)).json();
-    suggestions.innerHTML = "";
-    let cities = result._embedded["city:search-results"];
-    let length = cities.length > 5 ? 5 : cities.length;
-    for (let i = 0; i < length; i++) {
-      let option = document.createElement("option");
-      option.value = cities[i].matching_full_name;
-      suggestions.appendChild(option);
-    }
-    console.log(result);
-  });
-  
+  })
+
   let updateCurrentWeather = (data) => {
-    city.textContent = data.name + ", " + data.sys.country;
+    console.log(data)
+    city.textContent = data.name + ', ' + data.sys.country;
     day.textContent = dayOfWeek();
-    humidity.textContent = data.main.humidity;
+    humidity.textContent = data.main.humidty;
     pressure.textContent = data.main.pressure;
     let windDirection;
     let deg = data.wind.deg;
-    if (deg > 45 && deg <= 135) {
-      windDirection = "East";
-    } else if (deg > 135 && deg <= 225) {
-      windDirection = "South";
-    } else if (deg > 225 && deg <= 315) {
-      windDirection = "West";
+    if(deg > 45 && deg <= 135) {
+      windDirection = 'East';
+    } else if(deg > 135 && deg <= 225) {
+      windDirection = 'South';
+    } else if(deg > 225 && deg <= 315) {
+      windDirection = 'West';
     } else {
-      windDirection = "North";
+      windDirection = 'North';
     }
-    wind.textContent = windDirection + ", " + data.wind.speed;
-    temperature.textContent =
-      data.main.temp > 0
-        ? "+" + Math.round(data.main.temp)
-        : Math.round(data.main.temp);
-  };
+    wind.textContent = windDirection + ', ' + data.wind.speed;
+    temperature.textContent = data.main.temp > 0 ? 
+                              '+' + Math.round(data.main.temp) : 
+                                 Math.round(data.main.temp);
+  }
   
   let dayOfWeek = () => {
-    return new Date().toLocaleDateString("en-US", { weekday: "long" });
-  };
+   return new Date().toLocaleDateString('en-EN', {'weekday': 'long'});
+  }
+
+ 
